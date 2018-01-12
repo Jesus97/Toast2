32a52783ff1f0fcba635bb8a8dcd8c112f4e1ec3
Jesus97 <jesusesma97@gmail.com>
2018-01-12 09:16:23 +0100

    1º commit

--- app/src/main/java/com/iesvirgendelcarmen/dam/toast2/Toast01.java
+++ app/src/main/java/com/iesvirgendelcarmen/dam/toast2/Toast01.java
@@ -2,9 +2,13 @@ package com.iesvirgendelcarmen.dam.toast2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -22,7 +26,15 @@ Button boton1;
         boton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Toast toast = new Toast(getApplicationContext());
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.toast,(ViewGroup) findViewById(R.id.layout));
+                toast.setView(layout);
+                toast.setGravity(Gravity.CENTER|Gravity.CENTER,0,0);
+                toast.setDuration(Toast.LENGTH_SHORT);
+                TextView mensaje = (TextView) layout.findViewById(R.id.textoLL);
+                mensaje.setText("NOTIFIQUEISION");
+                toast.show();
             }
         });
     }
